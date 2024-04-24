142c9f04132f3619f6d2e79fe54cbc089b10b3fe
gdotmount <gmaxmount@gmail.com>
2024-04-24 16:54:18 -0500

    edits for presentation; fixed error with how algorithm is processed

--- dashboard/backend/base.py
+++ dashboard/backend/base.py
@@ -1,14 +1,21 @@
 from flask import Flask
 from export_excel import output_groups
+from grouping import group_sort
 
 api = Flask(__name__)
+algo_run = False
 
 @api.route('/groups', methods=['GET'])
 def my_profile():
+    global algo_run
+    if not algo_run:
+        print("Calling algorithm")
+        group_sort()
+        print("ALgorithm run")
+        algo_run = True
     df = output_groups()
     df_json = df.to_json(orient='records')
     print(df)
-    print(df_json)
     return df_json
 
 @api.route('/project/<projectID>', methods=['GET'])
@@ -22,5 +29,11 @@ def project(projectID):
 
 @api.route('/get_excel', methods=['GET'])
 def get_excel():
+    global algo_run
+    if not algo_run:
+        print("Calling algorithm")
+        group_sort()
+        print("ALgorithm run")
+        algo_run = True
     output_groups(export=True)
     return {'result': 'success'}
