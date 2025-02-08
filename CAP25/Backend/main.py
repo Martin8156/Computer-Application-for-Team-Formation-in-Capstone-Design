afea78e3f133c27c64581526291f0c29309be98c
Erertertet <s2293297@ed.ac.uk>
2025-02-08 16:45:44 -0600

    add: solving in backend and handler for starting solver

--- CAP25/Backend/main.py
+++ CAP25/Backend/main.py
@@ -2,23 +2,35 @@ import os
 import tornado.ioloop
 import tornado.web
 import json
+import subprocess
+
 UPLOAD_FILE_DIR = "Files/"
 MOST_RECENT_FILE = "source"
+RES_FILE = "Files/out.json"
+STU_FILE = UPLOAD_FILE_DIR + "Student.csv"
+COM_FILE = UPLOAD_FILE_DIR + "Company.csv"
+
+
 class Base_Handler(tornado.web.RequestHandler):
     def prepare(self):
-        if self.request.remote_ip not in ['127.0.0.1', '::1']:
+        if self.request.remote_ip not in ["127.0.0.1", "::1"]:
             print(f"{self.request.remote_ip} blocked")
             self.send_error(403)
+
     def set_default_headers(self):
         self.set_header("Access-Control-Allow-Origin", "*")
         self.set_header("Access-Control-Allow-Headers", "x-requested-with")
-        self.set_header('Access-Control-Allow-Methods', 'POST, GET, OPTIONS')
+        self.set_header("Access-Control-Allow-Methods", "POST, GET, OPTIONS")
+
     def options(self):
         self.set_status(204)
         self.finish()
+
+
 class Main_Handler(Base_Handler):
     def get(self):
-        self.write('''
+        self.write(
+            """
         <html>
         <body>
         <form action="/file/upload" enctype="multipart/form-data" method="post">
@@ -31,56 +43,88 @@ class Main_Handler(Base_Handler):
         </form>
         </body>
         </html>
-        ''')
+        """
+        )
+
+
 class Upload_File_Handler(Base_Handler):
     def post(self):
         # Do the parsing to verify the file
         # Sendback the error message or needed file
         # Start new process of solving combination
-        fileinfo = self.request.files['filearg'][0]
+        fileinfo = self.request.files["filearg"][0]
         print("fileinfo is", fileinfo)
-        fname = fileinfo['filename']
+        fname = fileinfo["filename"]
         extn = os.path.splitext(fname)[1]
-        cname = MOST_RECENT_FILE + extn
-        fh = open(UPLOAD_FILE_DIR + cname, 'wb')
-        fh.write(fileinfo['body'])
+        cname = self.get_body_argument("file_type") + extn
+        fh = open(UPLOAD_FILE_DIR + cname, "wb")
+        fh.write(fileinfo["body"])
         self.finish(cname + " uploaded")
+
+
 class Current_Alloc_Handler(Base_Handler):
     def post(self):
         random_matching = {
             "students": [
-                {
-                    "name": "abcd", "eid": 1234, "skill_set": [2, 3]
-                }, {
-                    "name": "efgh", "eid": 6666, "skill_set": [0, 3]
-                }
+                {"name": "abcd", "eid": 1234, "skill_set": [2, 3]},
+                {"name": "efgh", "eid": 6666, "skill_set": [0, 3]},
             ],
             "projects": [
                 {
                     "name": "random project A",
                     "skill_req": [0, 1],
-                }, {
-                    "name": "random project B",
-                    "skill_req": [3]
-                }
+                },
+                {"name": "random project B", "skill_req": [3]},
             ],
             "skills": {
                 0: "AI",
                 1: "Analogue Circuit",
                 2: "Embedded",
-                3: "Operating System"
-            }
+                3: "Operating System",
+            },
         }
         self.write(json.dumps(random_matching))
-application = tornado.web.Application([
-    (r"/", Main_Handler),
-    (r"/file/upload", Upload_File_Handler),
-    # (r"/file/download"),
-    (r"/matching", Current_Alloc_Handler),
-    (r"/action/load", Main_Handler),
-    # (r"/action/set_match"),
-    # (r"/action/delete_match"),
-])
+
+
+
+class Alloc_Solve_Handler(Base_Handler):
+    def post(self):
+        if not os.path.exists(RES_FILE):
+            self.write(json.dumps(
+                {"result": "err", "msg": "existing an ongoing solver"}
+            ))
+            return
+        
+        if not os.path.exists(STU_FILE):
+            self.write(json.dumps(
+                {"result": "err", "msg": "student file does not exist, please upload that first"}
+            ))
+            return
+        
+        if not os.path.exists(COM_FILE):
+            self.write(json.dumps(
+                {"result": "err", "msg": "company file does not exist, please upload that first"}
+            ))
+            return
+        
+        subprocess.Popen(['python', 'Backend/solver.py'])
+        self.write(json.dumps({"result": "success", "msg": "solver started"}))
+
+
+application = tornado.web.Application(
+    [
+        (r"/", Main_Handler),
+        (r"/file/upload", Upload_File_Handler),
+        # (r"/file/download"),
+        (r"/matching", Current_Alloc_Handler),
+        (r"/action/load", Main_Handler),
+        # (r"/action/set_match"),
+        # (r"/action/delete_match"),
+        (r"/action/solve", Alloc_Solve_Handler),
+    ]
+)
+
+
 if __name__ == "__main__":
     application.listen(8888)
     tornado.ioloop.IOLoop.instance().start()
